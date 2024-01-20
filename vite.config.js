5c7ebb91962e69cd2266e3fdea779526ba853631
yeralash <mishaneiman@gmail.com>
2024-01-20 17:39:33 +0200

    Updated vite config and deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Use an environment variable for the base URL. If it's not set, default to '/'.
+  base: process.env.BASE_URL || '/',
+  build: {
+    outDir: 'dist'
+  }
 });
