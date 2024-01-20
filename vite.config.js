03eb5fe95cb2d9604e443456fc25c25377ab9298
yeralash <mishaneiman@gmail.com>
2024-01-20 17:34:55 +0200

    Update to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Use an environment variable for the base URL. If it's not set, default to '/'.
-  base: process.env.BASE_URL || '/',
 });
